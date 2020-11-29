35ec2ffb2fbd984ba6e6bc8c285946bcb0f8048b
mads5606 <mads5606@gmail.com>
2020-11-29 18:53:20 +0100

    Implemented handling og signals with proper closing of zookeeper
    Implemmented:
    - SIGTERM and SIGINT are trapped, zookeeper closed and the process exited with the exit-code from closing the zookeeper process.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,19 @@
 
 configure-zookeeper.sh
 
+function cleanup(){
+	/opt/zookeeper/bin/zkServer.sh stop
+	local e1=$?
+	echo "ZooKeeper Stopped"
+	exit $e1
+}
+
+trap cleanup SIGTERM SIGINT
+
 if [ $? != 0 ]; then
     exit 1
 fi
-
+echo ""
 echo "INFO Starting ZooKeeper"
 
 if ! [[ -z "$ZOO_AUTHENTICATION" ]]; then
@@ -15,4 +24,6 @@ if ! [[ -z "$ZOO_AUTHENTICATION" ]]; then
     fi
 fi
 
-/opt/zookeeper/bin/zkServer.sh start-foreground
+/opt/zookeeper/bin/zkServer.sh start-foreground &
+
+wait
